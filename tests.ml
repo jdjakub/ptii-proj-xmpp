a74dc3b41a0ae7c51deecbad7903cc64578267d6
jdjakub <jdj27@cam.ac.uk>
2017-05-12 01:48:33 +0100

    60 going on 70

--- tests.ml
+++ tests.ml
@@ -7,7 +7,7 @@ let select_random_all n () = Random.int n
 (* 445 chars *)
 let lipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-let n_senders = 50
+let n_senders = 70
 let n_recvers = n_senders
 
 module Raw = Xml.P.Raw
